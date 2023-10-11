d384c5142dfbb2a5f0b0151fe5134493185f1139
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-10-11 16:45:17 +0600

    commit bisto-boss bannner with setup

---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { FaUserCircle} from "react-icons/fa";
+import shoppingCart from '../../assets/icon/SCart.png'
+const Navbar = () => {
+   const navOptions = <>
+      <li><a>Hmome</a></li>
+      <li><a>Contact Us</a></li>
+      <li><a>Deshboard</a></li>
+      <li><a>Our Menu</a></li>
+      <li><a>Our Shop</a></li>
+   </>
+   return (
+      <div className="navbar bg-base-100">
+         <div className="navbar-start">
+            <div className="dropdown">
+               <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+               </label>
+               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                  {navOptions}
+               </ul>
+            </div>
+            <div className="normal-case">
+               <a className="text-xl block tracking-tight">BISTRO BOSS</a>
+               <a className="block tracking-widest text-center">RESTAURANT</a>
+            </div>
+
+         </div>
+         <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+               {navOptions}
+            </ul>
+         </div>
+         <div className="navbar-end">
+            <div className="relative">
+               <img className="w-12" src={shoppingCart} alt="" />
+            </div>
+            <button className="mx-3">Sign Out</button>
+            <div><FaUserCircle></FaUserCircle></div>
+         </div>
+      </div>
+   );
+};
+
+export default Navbar;
